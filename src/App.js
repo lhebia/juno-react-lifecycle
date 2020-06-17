0733f11f1ca3de7f53d350851f2147067f11ef54
lhebia <lhebia@gmail.com>
2020-06-17 15:24:16 -0400

    Initial commit of complete code-along

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      artwork: []
+    }
+  }
+
+  componentDidMount() {
+    axios({
+      url: 'https://www.rijksmuseum.nl/api/en/collection',      
+      method: 'GET',      
+      dataType: 'json',      
+      params: {        
+        key: '4H66F4YK',        
+        format: 'json'
+      }
+    }).then( (response) => {
+      console.log(response.data.artObjects);
+      this.setState({
+        artwork: response.data.artObjects
+      });
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Art Gallery</h1>
+
+        {
+          this.state.artwork.map((piece, index) => {
+            return (
+              <Fragment key={index}>
+                <h2>{piece.title}</h2>
+                <img src={piece.webImage.url} alt={piece.longTitle} />
+              </Fragment>
+            );
+          })
+        }
+      </div>
+    );
+
+  }
 }
 
 export default App;
